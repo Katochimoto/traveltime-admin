7f6fdaa2c6291b7e995d5c8cee9d9368b596e1f3
Anton Tursenev <rikishi@rikishi-pro.local>
2023-01-22 22:48:05 +0700

    Add sync route

--- src/api/article/controllers/article.ts
+++ src/api/article/controllers/article.ts
@@ -7,12 +7,17 @@ import { factories } from '@strapi/strapi'
 export default factories.createCoreController('api::article.article', ({ strapi }) => ({
   async sync(ctx) {
     const { lastSync } = ctx.query;
-    const entries = await strapi.db.query('api::article.article').findMany({
-      where: {
-        updatedAt: { $gt: new Date(lastSync) },
-      },
-    });
+    const entries = await strapi.db.query('api::article.article').findMany();
 
     ctx.body = entries;
   },
 }));
+
+/**
+ *
+ * {
+      where: {
+        updatedAt: { $gt: new Date(lastSync) },
+      },
+    }
+ */
