75d3854b272acb42f2859c514b1d13ea441225b9
Anton Tursenev <rikishi@rikishi-pro.local>
2023-01-23 22:29:36 +0700

    Add sync endpoint

--- src/api/article/controllers/article.ts
+++ src/api/article/controllers/article.ts
@@ -6,13 +6,13 @@ import { factories } from '@strapi/strapi'
 
 export default factories.createCoreController('api::article.article', ({ strapi }) => ({
   async sync(ctx) {
-    const { lastSync, locale } = ctx.query;
-    const articles = await strapi.service('api::article.article').sync({ lastSync, locale });
+    const datetime = new Date().toISOString();
+    const articles = await strapi.service('api::article.article').sync(ctx.query);
     ctx.send({
       changes: {
         articles,
       },
-      datetime: new Date().toISOString(),
+      datetime,
     });
   },
 }));
--- src/api/sync/controllers/sync.ts
+++ src/api/sync/controllers/sync.ts
@@ -1,11 +1,12 @@
 export default {
   async sync(ctx) {
+    const datetime = new Date().toISOString();
     const articles = await strapi.service('api::article.article').sync(ctx.query);
     ctx.send({
       changes: {
         articles,
       },
-      datetime: new Date().toISOString(),
+      datetime,
     });
   },
 }
