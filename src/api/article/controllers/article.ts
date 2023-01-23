8a1bbff826d21d43d97b035704b8107eaf6eb684
Anton Tursenev <rikishi@rikishi-pro.local>
2023-01-23 22:27:49 +0700

    Add sync endpoint

--- src/api/article/controllers/article.ts
+++ src/api/article/controllers/article.ts
@@ -8,8 +8,11 @@ export default factories.createCoreController('api::article.article', ({ strapi 
   async sync(ctx) {
     const { lastSync, locale } = ctx.query;
     const articles = await strapi.service('api::article.article').sync({ lastSync, locale });
-    ctx.send(articles);
-    // const sanitizedEntries = await this.sanitizeOutput(articles, ctx);
-    // return this.transformResponse(sanitizedEntries);
+    ctx.send({
+      changes: {
+        articles,
+      },
+      datetime: new Date().toISOString(),
+    });
   },
 }));
